dbfcb51ac2dcdddf0fcef9c86b1cf6d624271127
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-04-30 11:08:18 -0300

    feat: agregar funcion moverTarea

--- ejercicio.c
+++ ejercicio.c
@@ -20,6 +20,7 @@ Tarea *crearTarea(int id, char *desc, int dura);
 void insertarNodo(Nodo **head, Tarea *tarea);
 void mostrarTareas(Nodo **head);
 int generadorID(Nodo **head);
+void moverTarea(Nodo **pendientes, Nodo **realizadas, int id);
 
 int main()
 {
@@ -33,7 +34,43 @@ int main()
     Tarea *tarea2 = crearTarea(generadorID(&listasPendientes), "Tarea a hacer 2", 120);
     insertarNodo(&listasPendientes, tarea2);
 
-    mostrarTareas(&listasPendientes);
+    int opcion;
+    do
+    {
+        printf("\nMenú:\n");
+        printf("1. Mostrar tareas pendientes\n");
+        printf("2. Mostrar tareas realizadas\n");
+        printf("3. Mover tarea de pendientes a realizadas\n");
+        printf("4. Salir\n");
+        printf("Ingrese su opción: ");
+        scanf("%d", &opcion);
+        fflush(stdin); // Limpiar el búfer del teclado
+
+        switch (opcion)
+        {
+        case 1:
+            printf("\nTareas Pendientes:\n");
+            mostrarTareas(&listasPendientes);
+            break;
+        case 2:
+            printf("\nTareas Realizadas:\n");
+            mostrarTareas(&ListasRealizadas);
+            break;
+        case 3:
+            printf("\nIngrese el ID de la tarea a mover de pendientes a realizadas: ");
+            int id;
+            scanf("%d", &id);
+            fflush(stdin);
+            moverTarea(&listasPendientes, &ListasRealizadas, id);
+            break;
+        case 4:
+            printf("\nSaliendo del programa...\n");
+            break;
+        default:
+            printf("\nOpción no válida. Inténtelo de nuevo.\n");
+            break;
+        }
+    } while (opcion != 4);
 
     return 0;
 }
@@ -90,5 +127,34 @@ int generadorID(Nodo **head)
         }
         id++;
         return id;
+    }
+}
+
+void moverTarea(Nodo **pendientes, Nodo **realizadas, int id)
+{
+    Nodo *prev = NULL;
+    Nodo *actual = *pendientes;
+
+    while (actual != NULL && actual->T.TareaID != id)
+    {
+        prev = actual;
+        actual = actual->Siguiente;
+    }
+
+    if (actual != NULL)
+    {
+        if (prev != NULL)
+            prev->Siguiente = actual->Siguiente;
+        else
+            *pendientes = actual->Siguiente;
+
+        actual->Siguiente = *realizadas;
+        *realizadas = actual;
+
+        printf("Tarea movida de pendientes a realizadas.\n");
+    }
+    else
+    {
+        printf("No se encontró una tarea con el ID especificado en la lista de pendientes.\n");
     }
 }
